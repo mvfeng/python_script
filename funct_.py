9b5708695227df2837ed94829fdce7c58fe87473
ifeng <65393910+mvfeng@users.noreply.github.com>
2023-11-29 15:09:29 +0800

    Update funct_.py

--- funct_.py
+++ funct_.py
@@ -9,6 +9,7 @@ import time
 from config_ import *
 # import msvcrt
 import logging
+# from sendmail import Mail
 
 # def log_init(name_):
 #     log_filename = LOG_DIR + name_ + ".log"
@@ -19,7 +20,7 @@ import logging
 def log_1(name_):
     # 定义文件
     file1 = logging.FileHandler(filename=str(name_)+'.log', mode='a', encoding='utf-8')
-    fmt = logging.Formatter(fmt="%(asctime)s - %(name)s - %(levelname)s -%(module)s:  %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
+    fmt = logging.Formatter(fmt="%(asctime)s %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
     file1.setFormatter(fmt)
 
     # 定义日志
@@ -30,13 +31,23 @@ def log_1(name_):
 def log_2(name_):
     # 定义文件
     file2 = logging.FileHandler(filename=str(name_)+'.log', mode='a', encoding='utf-8')
-    fmt = logging.Formatter(fmt="%(asctime)s - %(name)s - %(levelname)s -%(module)s:  %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
+    fmt = logging.Formatter(fmt="%(asctime)s %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
     file2.setFormatter(fmt)
 
     # 定义日志
     logger2 = logging.Logger(name='log_2', level=logging.INFO)
     logger2.addHandler(file2)
     return logger2
+
+
+def anykey_go():
+    print("自动模式/提前连好xxx")
+    # while True:
+    #     key=input("如果您已经完成按 'D' 或 'd' 继续下一步...\n")
+    #     if key == "D":
+    #         break
+    #     elif key == "d":
+    #         break
 
 # uart串口交互功能函数
 def serial_open(port, bps, timeouts):
@@ -47,7 +58,7 @@ def serial_cmd(cmd):
     # print(serial_obj.is_open)
     for line in cmd:
         serial_obj.write(line.encode("utf-8"))  # 写数据
-        time.sleep(1)
+        time.sleep(0.01) # 必须添加
     r_data = serial_obj.readlines()
     rdata = []
     for i in r_data:
@@ -71,6 +82,11 @@ def ssh_connect(host_ip,host_port,host_name,host_password,cmd):
 
 def ssh_cmd(cmd):
     ssh_result=ssh_connect(HOST_IP,HOST_PORT,HOST_USERNAME,HOST_PASSWORD,cmd)
+#   print("ssh connect success!")
+    return ssh_result
+
+def ssh_cmd2(cmd):
+    ssh_result=ssh_connect(HOST_IP2,HOST_PORT2,HOST_USERNAME2,HOST_PASSWORD2,cmd)
 #   print("ssh connect success!")
     return ssh_result
 
@@ -100,6 +116,17 @@ def power_monitor():
             print("reboot success! port 22 is open! ")
             break
         else:
-            print("服务器正在启动中......")
+            print("The device is restarting...")
 
 
+
+# 邮件通知功能
+def mail_note():
+    # 定义邮件参数内容
+    msg = '测试已经over,请注意查看'   # 邮件正文f
+    title = '《测试报告》'    # 邮件标题
+    receivers = ['admin@qq.com']   # 邮件接收者
+    attachment = [r"C:\Users\readme.md"]    # 附件仅支持zip/pdf/xlsx/docx后缀文件
+
+    # 通过邮件发送最新的报告
+    Mail().sendmail(receivers, title, msg, attachment)
